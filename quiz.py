fccdbd6695d293a2e13a17ef8017a316efebeb65
KRDen-o <axel.shepherdwilliam@horowhenua.school.nz>
2025-03-17 10:42:59 +1300

    Add files via upload
    more questions and answers

--- quiz.py
+++ quiz.py
@@ -59,17 +59,32 @@ def play_again():
     if response == "YES":
         return True
     else:
+        print("either you can't spell or your leaving")
         return False
+
 #this is where the questions will live
 questions = {
     "what is johnathan joestar main power?": "B",
     "what is joseph joestar main ability he uses?": "D",
-    "what is jotaro kujo's stand name?": "A"
+    "what is jotaro kujo's stand name?": "A",
+    "How about josuke from part 4 stand name?": "C",
+    "how about some jojo villians?": "A",
+    "Dio Brando main power/part one?": "A",
+    "what group does Kars belong to?": "B",
+    "did Dio return and with what stand?": "C",
+    "who was the real villian of part 4?": "D",
 }
 
 options = [["A. Stand", "B. Hamon", "C. Vampiric powers", "D. Other"],
            ["A. Stand", "B. Hamon", "C. weapons", "D. A and B"],
-           ["A. Star platinum", "B. The world", "C. The wonder of U", "D. Other"],]
+           ["A. Star platinum", "B. The world", "C. The wonder of U", "D. Other"],
+           ["A. crazed dimond", "B. soft and wet", "C. crazy dimond", "D. nut and bolt"],
+           ["A. sure","B. you can't get this wrong","C. i mean the question", "D. the player is gay"],
+           ["A. vampire powers", "B. Hamon", "C. ZA WADO", "D. Pillar men"],
+           ["A. Dio's ghouls", "B. The pillar men", "C. Vampire's", "D. Locacaca Organization"],
+           ["A. yes: ZA HANDO", "B. no: he's mega dead", "C. yes and he has the the world", "no: hes over heaven"],
+           ["A. Toru", "B. Josefumi Kujo", "C. The head doctor", "D. Yoshikage kira"]
+           ]
 
 new_game()
 
